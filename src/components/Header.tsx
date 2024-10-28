ab8c822f1273b362d3414fb479ca1b937112798e
Juwon Jeong <2jw0718@gmail.com>
2024-10-29 07:12:07 +0900

    Header 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,31 @@
-import { View } from "react-native";
+import EvilIcons from "@expo/vector-icons/EvilIcons";
+import Icon from "@expo/vector-icons/Ionicons";
+import { Text, View } from "react-native";
 
 const Header = () => {
-  return <View style={{ borderWidth: 1 }}></View>;
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        alignItems: "center",
+        height: 50,
+        paddingHorizontal: 20,
+        marginBottom: 20,
+      }}
+    >
+      <Icon name="arrow-back" size={30} />
+      <View style={{ flex: 1 }}>
+        <Text
+          style={{
+            textAlign: "center",
+          }}
+        >
+          Header
+        </Text>
+      </View>
+      <EvilIcons name="gear" size={24} color="black" />
+    </View>
+  );
 };
 
 export default Header;
